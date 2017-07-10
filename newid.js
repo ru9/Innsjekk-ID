c3441cf9911a012787ce135827988011b4a5813f
ru9 <runisunde@gmail.com>
2017-07-10 06:03:45 +0200

    test sync

--- newid.js
+++ newid.js
@@ -1,6 +1,7 @@
 //GETing stored variable Card ID
 var cardId = localStorage.getItem("cardId");
 
+
 //Storing and sending info to node
 function testResults(form) {
   var fullName = form.fullname.value;
