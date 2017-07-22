bd0f8587c459c876daee6b3a7176b8666532b7b0
ru9 <runisunde@gmail.com>
2017-07-22 22:16:03 +0200

    Before request imp

--- newid.js
+++ newid.js
@@ -1,11 +1,30 @@
-//GETing stored variable Card ID
 var cardId = localStorage.getItem("cardId");
+//var request = require('request');
 
 //Storing and sending info to node
 function testResults(form) {
   var fullName = form.fullname.value;
   var companyName = form.companyname.value;
 
+  /*  let i = {
+      method: 'POST',
+      uri: 'http://localhost:8080/api/worker/',
+      body: {
+        "name": fullName,
+        "company": companyName,
+        "cardid": cardId,
+        "checkedIn": false
+      },
+      json: true
+    }
+
+    request(i)
+      .then(function(response) {
+        console.log(response)
+      })
+      .catch(function(err) {
+        console.log(err)
+      }) */
   var xhr = new XMLHttpRequest();
   xhr.open('POST', "http://localhost:8080/api/worker/", true);
   xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
